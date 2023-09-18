c9f77c64a72aa65c920e5fefabeb21a3c707be28
MrFunreal <62206882+Mrfunreal@users.noreply.github.com>
2023-09-18 13:02:09 +0200

    Update Batch_Convert_Mp3_To_Wav.bat

--- FFMPEG/Batch_Convert_Mp3_To_Wav.bat
+++ FFMPEG/Batch_Convert_Mp3_To_Wav.bat
@@ -1,7 +1,7 @@
-:: This FFMPEG script can convert audio from MP3 or WAV
-:: Drop this .bat into a folder full of MP3 files, double click it, an dwait to for it to finish.
+:: This FFMPEG script Batch converts MP3 to Wav.
+:: Drop this .bat into a folder full of MP3 files, double click it, and wait to for it to finish.
 :: It will delete all MP3 files when it's done.
-::It wil lremain open spewing out a list of all files inside the folder. Should you want to copy the list of names for something.
+:: It will remain open with a printed list of all files inside the folder. Should you want to copy the list of names for something.
 
 for %%i in (*.mp3) do ffmpeg  -i "%%i" -ar 44100 "%%~ni.wav"
 del  *.mp3
