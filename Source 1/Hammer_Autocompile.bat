2f3d2395bfc021095ff16b9b2d486ddbfd93d6c4
MrFunreal <62206882+Mrfunreal@users.noreply.github.com>
2025-07-27 17:41:56 +0200

    Update Hammer_Autocompile.bat
    Reworked this thing entirely

--- Source 1/Hammer_Autocompile.bat
+++ Source 1/Hammer_Autocompile.bat
@@ -1,183 +1,158 @@
 @echo off
 cls
-::	Autocompile bat file to batch compile multiple files.
-::	SET the Game_path and Game_subfolder_name. Maps will compile into the Game_subfolder_name's "Maps" subfolder.
-::	This bat file pauses when its done, so the CMD window can be used to read errors.
-::	New map compiles start with a red box. VSBP is Yellow, VVIS is Purple, VRAD is Cyan, xcopy is Green. To easily see what step you're on.
+::	This is an autocompiler bat file for source engine.
+::	Instead of using hammer to compile one map at a time, you can compile multiple automatically while hammer isn't needed.
+::	Save this .bat file anywhere you want, define the SET commands in VARIABLES, pick compile settings and run this file.
+::---------------------------------------------------------------VARIABLES---------------------------------------------------------------
+::	Where the game executable is.
+SET Game_path=D:\programme\steam\steamapps\common\left 4 dead 2
 
-:: TIP: Personally, I saved this .bat in a mapping folder where all my projects are, with a shortcut on the desktop.
-::		I right click the shortcut to edit this file for my next project to compile multiple maps while i sleep or do chores.
+::	Where the Gameinfo.txt is. Gameinfo is read for custom mounted content. Lights.rad also needs to be here, if you have one.
+::	Could define custom folder, but the game moves the bsp after compiling cubemaps/stringtables.
+::	Using the base folder is less confusing, as it won't force stale maps to be loaded.
+SET Gameinfo_path=left4dead2
 
-	::Where the game executable is.
-	SET Game_path=D:\programme\steam\steamapps\common\left 4 dead 2
-	
-	::Which subfolder of the above mentioned folder the maps will go to. Default is "left4dead2"
-	::WARNING: THIS FOLDER REQUIRES A GAMEINFO! THIS GAMEINFO WILL ALSO NEED TO POINT TO CUSTOM CONTENT!!!
-	::WARNING: MAKE SURE THIS STRING DOES NOT END WITH A SPACE!
-	SET Game_subfolder_name=left4dead2
-	
-	::VMF Location and name, without extension. Add more maps or locations if you want, modify this file accordingly
-	::Once a VMF is compiled, it will do the next listed VMF. If no VMF's are listed, the compile will end.
-	SET VMF_Location=F:\Modding\L4D1_Mapping\the last stand
-	SET VMF1_Name=c14m1_junkyard_l4d
-	SET VMF2_name=
-	SET VMF3_name=
-	SET VMF4_name=
-	SET VMF5_name=
-	SET VMF6_name=
-	SET VMF7_name=
-	SET VMF8_name=
-	SET VMF9_name=
-	SET VMF10_name=
-	
-:: COMPILE SETTINGS. Work with comments to define which settings you would like to use.
-:: Currently uses "Hdr Fast preset" as it is not commented out.
-:: VBSP options https://developer.valvesoftware.com/wiki/VBSP
-:: VVIS options https://developer.valvesoftware.com/wiki/VVIS
-:: VRAD options https://developer.valvesoftware.com/wiki/VRAD	
+::	Where your VMF files are.
+SET	VMF_Location=F:\Modding\L4D2_Mapping\TUMTaRA\!SOURCE
+::	List of maps to compile, separated by space, no file ending. 
+Set	MAPLIST=tumtara_empty
+::	MAPLIST=c1m1_hotel c1m2_streets c1m3_mall c1m4_atrium
+
+::-----------------------------------------------------------COMPILE SETTINGS------------------------------------------------------------
+::	Set up your own presets, then comment out the ones you do not want. Change back and forth if needed.
+::	Tip: In Notepad++, you can do Shift+Alt+LMB to select vertically and them move the :: around.
+::	Add your own presets if you want. It's exactly Like hammer's Expert compile window, just without UI.
+
+::	VBSP: https://developer.valvesoftware.com/wiki/VBSP
+::	VVIS: https://developer.valvesoftware.com/wiki/VVIS
+::	VRAD: https://developer.valvesoftware.com/wiki/VRAD	
 
 	::HDR FINAL PRESET
-::	SET vbsp_setting=-threads 22
-::	SET vvis_setting=-threads 22
-::	SET vrad_Setting=-threads 22 -hdr -TextureShadows -staticproplighting -StaticPropPolys -final
+	SET vbsp_setting=-threads 22
+	SET vvis_setting=-threads 22
+	SET vrad_Setting=-threads 22 -hdr -TextureShadows -staticproplighting -StaticPropPolys -final
 	
 	::HDR FAST PRESET
-	SET vbsp_setting=-threads 22
-	SET vvis_setting=-threads 22 -fast
-	SET vrad_Setting=-threads 22 -hdr -textureshadows -StaticPropLighting -StaticPropPolys	
+::	SET vbsp_setting=-threads 22
+::	SET vvis_setting=-threads 22 -fast
+::	SET vrad_Setting=-threads 22 -hdr -textureshadows -StaticPropLighting -StaticPropPolys	
 
 	::DIRTY FAST PRESET
 ::	SET vbsp_setting=-threads 22 
 ::	SET vvis_setting=-threads 22 -fast
 ::	SET vrad_Setting=-threads 22 -fast
 
-:: DO NOT EDIT ANYTHING BELOW HERE
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::: !!! DO NOT EDIT ANYTHING BELOW HERE !!! ::::::::::::::::::::::::::::::::::::::::::::::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-	echo [101;93m Now compiling %VMF1_Name%[0m
-	echo [93m VBSP for %VMF1_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF1_Name%.vmf"
-	echo [95m VVIS for %VMF1_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF1_Name%.vmf"
-	echo [96m VRAD for %VMF1_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF1_Name%.vmf"
-	echo [92m COPY for %VMF1_Name% 
-	xcopy "%VMF_Location%\%VMF1_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF2_name (goto map2) ELSE (goto Donezo)
+::	Get current time, before compiling all maps.
+set "start_time=%TIME: =0%"
 
-	:Map2
-	echo [101;93m Now compiling %VMF2_Name%[0m
-	echo [93m VBSP for %VMF2_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF2_Name%.vmf"
-	echo [95m VVIS for %VMF2_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF2_Name%.vmf"
-	echo [96m VRAD for %VMF2_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF2_Name%.vmf"
-	echo [92m COPY for %VMF2_Name% 
-	xcopy "%VMF_Location%\%VMF2_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF3_name (goto map3) ELSE (goto Donezo)
+::	Compile steps looped as many times as needed.
+for %%i in (%MAPLIST%) do (
 
-	:Map3
-	echo [101;93m Now compiling %VMF3_Name%[0m
-	echo [93m VBSP for %VMF3_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF3_Name%.vmf"
-	echo [95m VVIS for %VMF3_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF3_Name%.vmf"
-	echo [96m VRAD for %VMF3_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF3_Name%.vmf"
-	echo [92m COPY for %VMF3_Name% 
-	xcopy "%VMF_Location%\%VMF3_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF4_name (goto map4) ELSE (goto Donezo)
+::	Calls for the current time to see when compile began. QOL for long batch compiles, like an entire campaign.
+	call :EchoCurrentTime "%%i"
 
-	:Map4
-	echo [101;93m Now compiling %VMF4_Name%[0m
-	echo [93m VBSP for %VMF4_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF4_Name%.vmf"
-	echo [95m VVIS for %VMF4_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF4_Name%.vmf"
-	echo [96m VRAD for %VMF4_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF4_Name%.vmf"
-	echo [92m COPY for %VMF4_Name% 
-	xcopy "%VMF_Location%\%VMF4_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF5_name (goto map5) ELSE (goto Donezo)
-
-	:Map5
-	echo [101;93m Now compiling %VMF5_Name%[0m
-	echo [93m VBSP for %VMF5_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF5_Name%.vmf"
-	echo [95m VVIS for %VMF5_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF5_Name%.vmf"
-	echo [96m VRAD for %VMF5_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF5_Name%.vmf"
-	echo [92m COPY for %VMF5_Name% 
-	xcopy "%VMF_Location%\%VMF5_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF6_name (goto map6) ELSE (goto Donezo)
-
-	:Map6
-	echo [101;93m Now compiling %VMF6_Name%[0m
-	echo [93m VBSP for %VMF6_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF6_Name%.vmf"
-	echo [95m VVIS for %VMF6_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF6_Name%.vmf"
-	echo [96m VRAD for %VMF6_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF6_Name%.vmf"
-	echo [92m COPY for %VMF6_Name% 
-	xcopy "%VMF_Location%\%VMF6_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF7_name (goto map7) ELSE (goto Donezo)
+	echo. [93m 
+	echo -------------------------------------------------------------------------------------------
+	echo VBSP for %%i 
+	echo -------------------------------------------------------------------------------------------
+	call :EchoTitleVbsp "%%i"
+	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Gameinfo_path%" "%VMF_Location%\%%i.vmf"
 	
-	:Map7
-	echo [101;93m Now compiling %VMF7_Name%[0m
-	echo [93m VBSP for %VMF7_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF7_Name%.vmf"
-	echo [95m VVIS for %VMF7_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF7_Name%.vmf"
-	echo [96m VRAD for %VMF7_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF7_Name%.vmf"
-	echo [92m COPY for %VMF7_Name% 
-	xcopy "%VMF_Location%\%VMF7_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF8_name (goto map8) ELSE (goto Donezo)
-
-	:Map8
-	echo [101;93m Now compiling %VMF8_Name%[0m
-	echo [93m VBSP for %VMF8_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF8_Name%.vmf"
-	echo [95m VVIS for %VMF8_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF8_Name%.vmf"
-	echo [96m VRAD for %VMF8_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF8_Name%.vmf"
-	echo [92m COPY for %VMF8_Name% 
-	xcopy "%VMF_Location%\%VMF8_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF9_name (goto map9) ELSE (goto Donezo)
-
-	:Map9
-	echo [101;93m Now compiling %VMF9_Name%[0m
-	echo [93m VBSP for %VMF9_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF9_Name%.vmf"
-	echo [95m VVIS for %VMF9_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF9_Name%.vmf"
-	echo [96m VRAD for %VMF9_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF9_Name%.vmf"
-	echo [92m COPY for %VMF9_Name% 
-	xcopy "%VMF_Location%\%VMF9_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	IF DEFINED VMF10_name (goto map10) ELSE (goto Donezo)
-
-	:Map10
-	echo [101;93m Now compiling %VMF10_Name%[0m
-	echo [93m VBSP for %VMF10_Name% 
-	"%Game_path%\bin\vbsp.exe" %vbsp_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF10_Name%.vmf"
-	echo [95m VVIS for %VMF10_Name% 
-	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF10_Name%.vmf"
-	echo [96m VRAD for %VMF10_Name% 
-	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Game_subfolder_name%" "%VMF_Location%\%VMF10_Name%.vmf"
-	echo [92m COPY for %VMF10_Name% 
-	xcopy "%VMF_Location%\%VMF10_Name%.bsp" "%Game_path%\%Game_subfolder_name%\maps" /y
-	goto Donezo
-
-:Donezo
-::	Makes CMD stay open even if you press a key.
+	echo. [95m 
+	echo -------------------------------------------------------------------------------------------
+	echo VVIS for %%i 
+	echo -------------------------------------------------------------------------------------------
+	call :EchoTitleVvis "%%i"
+	"%Game_path%\bin\vvis.exe" %vvis_setting% -game "%Game_path%\%Gameinfo_path%" "%VMF_Location%\%%i.vmf"
+	
+	echo. [96m
+	echo -------------------------------------------------------------------------------------------
+	echo VRAD for %%i 
+	echo -------------------------------------------------------------------------------------------
+	call :EchoTitleVrad "%%i"
+	"%Game_path%\bin\vrad.exe" %vrad_Setting% -game "%Game_path%\%Gameinfo_path%" "%VMF_Location%\%%i.vmf"
+	
+	echo. [92m
+	echo -------------------------------------------------------------------------------------------
+	echo COPY for %%i 
+	xcopy "%VMF_Location%\%%i.bsp" "%Game_path%\%Gameinfo_path%\maps" /y
+	echo -------------------------------------------------------------------------------------------
 	echo.
-	echo [93m///////////////////////////////////////////////////////////////////////////////
-	echo ---------------------------------- All done! ----------------------------------
-	echo ///////////////////////////////////////////////////////////////////////////////[0m
-	echo.
+)
 
-	cmd /k
+:: All of this is just to calculate the time to see how long the entire compile of all maps took.
+
+:: Capture current time after all compiles are done.
+set "end_time=%TIME: =0%"
+
+:: Parse start time into hours, minutes, seconds, hundredths
+for /F "tokens=1-4 delims=:." %%a in ("%start_time%") do (
+    set /A "start_h=1%%a-100"
+    set /A "start_m=1%%b-100"
+    set /A "start_s=1%%c-100"
+)
+
+:: Parse end time into hours, minutes, seconds, hundredths
+for /F "tokens=1-4 delims=:." %%a in ("%end_time%") do (
+    set /A "end_h=1%%a-100"
+    set /A "end_m=1%%b-100"
+    set /A "end_s=1%%c-100"
+)
+
+:: Convert start and mid time to total seconds
+set /A "start_total=start_h*3600 + start_m*60 + start_s"
+set /A "end_total=end_h*3600 + end_m*60 + end_s"
+
+:: Calculate elapsed seconds
+set /A "elapsed=end_total - start_total"
+
+:: Calculate hours, minutes, and seconds from elapsed total seconds
+set /A hours=elapsed / 3600
+set /A minutes=(elapsed %% 3600) / 60
+set /A seconds=elapsed %% 60
+
+:: Format to always show two digits (e.g., 01:05:09)
+if %hours% LSS 10 set hours=0%hours%
+if %minutes% LSS 10 set minutes=0%minutes%
+if %seconds% LSS 10 set seconds=0%seconds%
+
+:: Extract only HH:MM:SS from start_time and end_time
+set "start_hms=%start_time:~0,8%"
+set "end_hms=%end_time:~0,8%"
+
+::Finished text
+Title Autocompiler Finished
+echo.[33m:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo :                                  Autocompiler Finished                                  :
+echo :                        Begin Time: %start_hms% ^| End Time: %end_hms%                        :
+echo :                              Total elapsed time: %hours%:%minutes%:%seconds%                               :
+echo :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+echo.[0m
+goto end
+
+:end
+timeout /t 99999 >nul
+goto end
+
+:: Getting "Current time" for each map that begins compiling.
+:EchoCurrentTime
+Echo.
+echo [31m###########################################################################################
+echo Beginning Compile for %1. Current Time: %time:~-11,2%:%time:~-8,2%:%time:~-5,2%
+echo ###########################################################################################[0m
+exit /b
+
+:EchoTitleVbsp
+Title Compiling %1 - VBSP - Began %time:~-11,2%:%time:~-8,2%:%time:~-5,2%
+exit /b
+:EchoTitleVvis
+Title Compiling %1 - VVIS - Began %time:~-11,2%:%time:~-8,2%:%time:~-5,2%
+exit /b
+:EchoTitleVrad
+Title Compiling %1 - VRAD - Began %time:~-11,2%:%time:~-8,2%:%time:~-5,2%
+exit /b
+
